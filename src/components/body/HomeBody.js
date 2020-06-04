8ad479dcb2550dc854c8386ced8fa57513f38c03
fuser1988 <sergioluke35@gmail.com>
2020-06-03 23:25:50 -0300

    cambio estilos css

--- src/components/body/HomeBody.js
+++ src/components/body/HomeBody.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Container, Button } from "reactstrap";
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import { UserContext } from "context/UserContext.js";
+import { useHistory } from "react-router";
+import { useUsuarioService } from "services/UsuarioService.js";
+import Referencia from "model/Referencia.js";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../../toast.css';
 
-import SearchComponent from "components/search/SearchComponent.js";
+const HomeBody = () => {
+    const { push } = useHistory();
+    const { buscarUsuario } = useUsuarioService();
+    const { user, setUser } = useContext(UserContext);
 
-class HomeBody extends React.Component {
-    render() {
-        return (
+    React.useEffect(() => {
+
+        return () => {
+
+        }
+    }, []);
+
+    const notificar = (mensaje) => toast(mensaje, {
+        className: 'black-background',
+        bodyClassName: "grow-font-size",
+        progressClassName: 'fancy-progress-bar'
+    });
+
+    const respuestaDeLoginConFaceboook = (response) => {
+        let referencia = new Referencia(response);
+        notificar("Hola " + response.name + " estamos procesando tus datos.");
+        setUser(response);
+        buscarUsuario(referencia)
+        .then(((usuario) => {
+            setUser(usuario);
+            procesarRespuestaDelBackend(usuario);
+        }))
+        
+        const procesarRespuestaDelBackend = (usuario) => {
+            
+            if (usuario.tipoUsuario === "REGISTRADO_SIN_CONFIRMACION") {
+                push("/confirmaciones-de-cuentas");
+            } 
+        }
+    }
+
+    const falloLogin = () => {
+        setUser(null);
+    }
+
+
+    return (
+        <>
+            <ToastContainer />
             <div
                 className="page-header section-dark"
                 style={{
@@ -14,21 +61,44 @@ class HomeBody extends React.Component {
                         "url(" + require("assets/img/ecenario2.png") + ")"
                 }}
             >
-                <Container>
-                   <div className="content-center brand">
-                     <h1 className="h1-seo presentation-title">CIRCUITO</h1>
-                     <h1 className="h1-seo presentation-title">DEL UNDER</h1>
-                     <h3 className="d-none d-sm-block">
-                       Comparti a cada momento lo mejor de la musica.
-                     </h3>
-                     <SearchComponent/>
-                     {/* <Button>Bandas</Button>
-                     <Button>Recitales</Button> */}
-                   </div>
-                 </Container>
+                <div className="content-center top-center">
+                    <Container>
+                        <h1 className="tituloHome">Comparti donde estes lo mejor de la musica under.</h1>
+                        <p className=" subtituloHome">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five .
+                            </p>
+                        <div className="mt-sm-5 ">
+                            <Button className="col-sm-4 col-md-3 sm-mt btn-small" href="/BandasPage">Bandas</Button>
+                            <Button className="col-sm-4 col-md-3 btn-small" href="/RecitalesPage" >Recitales</Button>
+                        </div>
+
+                        {!user && (
+                            <div className="row d-flex justify-content-end align-items-center mt-4">
+                                <div className="mobile-align">
+                                    <p className="pie-pagina-font mr-2">Registrá tu banda a través Facebook.</p>
+                                </div>
+                                <div className="justify-content-center mobile-align">
+                                    <FacebookLogin
+                                        appId="222983722267666"
+                                        onFailure={falloLogin}
+                                        callback={respuestaDeLoginConFaceboook}
+                                        fields="name,email,picture"
+                                        render={renderProps => (
+                                            <Button id="tooltip" className="btn-icon btn-round btn btn-facebook btn-lg" onClick={renderProps.onClick}>
+                                                <i className="fab fa-facebook-square" />
+                                            </Button>
+                                        )}
+                                        />
+                                    </div>
+
+                            </div>
+                        )}                      
+                </Container>
+                </div>
             </div>
-        );
-    }
+        </>
+    );
+
 }
 
 export default HomeBody;
