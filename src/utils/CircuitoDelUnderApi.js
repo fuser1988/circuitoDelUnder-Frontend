666cac3ccb06f42fd19abd2c2db50e08f79d699e
fuser1988 <sergioluke35@gmail.com>
2020-06-15 19:47:09 -0300

    modifico conf base de datos

--- src/utils/CircuitoDelUnderApi.js
+++ src/utils/CircuitoDelUnderApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/circuito_under",
+  baseURL: "https://circuito-del-under-frontend.herokuapp.com/circuito_under",
   responseType: "json" // default
 });
