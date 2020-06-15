66bdef87ea93b2656a4e1707ceff166fe1c3de4d
fuser1988 <sergioluke35@gmail.com>
2020-06-15 20:28:07 -0300

    corrijo url backend para hacer pruebas en heroku

--- src/utils/CircuitoDelUnderApi.js
+++ src/utils/CircuitoDelUnderApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://circuito-del-under-frontend.herokuapp.com/circuito_under",
+  baseURL: "https://circuito-del-under-backend.herokuapp.com",
   responseType: "json" // default
 });
