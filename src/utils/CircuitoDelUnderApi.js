040e2555cf4a1eba847a7036076fa2699354470d
fuser1988 <sergioluke35@gmail.com>
2020-07-03 17:26:14 -0300

    ""

--- src/utils/CircuitoDelUnderApi.js
+++ src/utils/CircuitoDelUnderApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://circuito-del-under-backend.herokuapp.com//circuito_under",
+  baseURL: "https://circuito-del-under-backend.herokuapp.com/circuito_under",
   responseType: "json" // default
 });
